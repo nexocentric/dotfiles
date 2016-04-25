f1920160259498768271ae25252ffb7e2a7ff226
Dodzidenu Y. Dzakuma <nexocentric@gmail.com>
2016-04-25 00:56:19 -0500

    Added some configuration information for powerline.

--- installers/powerline.sh
+++ installers/powerline.sh
@@ -2,7 +2,17 @@
 
 print_stderr "Running installation for powerline."
 
+#you might need the current directory here....
+
 apt-get -y install powerline
 readonly INSTALLATION_STATUS=$?
+
+mkdir -p /home/${SCRIPT_INVOKER_USERNAME}/.config/fontconfig/conf.d/
+cd /home/${SCRIPT_INVOKER_USERNAME}/.config/fontconfig/conf.d/
+wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+
+mkdir -p /home/${SCRIPT_INVOKER_USERNAME}/.fonts/
+cd /home/${SCRIPT_INVOKER_USERNAME}/.fonts/
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
 
 exit $INSTALLATION_STATUS
